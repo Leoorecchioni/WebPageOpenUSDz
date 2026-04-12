53939d61942f0dbf31ac3e0c89d8679519a27207
Topo_ | Léo <91877186+Leoorecchioni@users.noreply.github.com>
2026-04-12 21:42:07 +0200

    [ADD] Fix bug vercel

---
+++ src/vite-env.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="vite/client" />
+
+declare module "*.usdz" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.glb" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gltf" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
